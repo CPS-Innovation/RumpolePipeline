08196297fa63f8a39a41ef64b73c021e923b9897
marty balmer <martybalmer@martys-MacBook-Air.local>
2022-05-06 15:11:07 +0100

    fixed document extraction

--- pdf-generator/Domain/Exceptions/PdfConversionException.cs
+++ pdf-generator/Domain/Exceptions/PdfConversionException.cs
@@ -5,7 +5,7 @@ namespace pdf_generator.Domain.Exceptions
 	public class PdfConversionException : Exception
 	{
 		public PdfConversionException(string documentId, string message) :
-			base($"Failed to convert document with id '{documentId}' to pdf. Exception: {message}.")
+			base($"Unable to convert document with id '{documentId}' to pdf. Exception: {message}.")
 		{
 		}
 	}
