6385c5a5a85e9926812848e72976010fcb5b9aa8
marty balmer <martybalmer@martys-air.home>
2022-05-18 13:10:49 +0100

    removed storage roles

--- terraform/storage-account.tf
+++ terraform/storage-account.tf
@@ -19,14 +19,14 @@ resource "azurerm_storage_container" "container" {
   container_access_type = "private"
 }
 
-resource "azurerm_role_assignment" "ra_blob_data_contributor" {
-  scope                = azurerm_storage_container.container.resource_manager_id
-  role_definition_name = "Storage Blob Data Contributor"
-  principal_id         = azurerm_function_app.fa_pdf_generator.identity[0].principal_id
-}
+# resource "azurerm_role_assignment" "ra_blob_data_contributor" {
+#   scope                = azurerm_storage_container.container.resource_manager_id
+#   role_definition_name = "Storage Blob Data Contributor"
+#   principal_id         = azurerm_function_app.fa_pdf_generator.identity[0].principal_id
+# }
 
-resource "azurerm_role_assignment" "ra_blob_data_reader" {
-  scope                = azurerm_storage_container.container.resource_manager_id
-  role_definition_name = "Storage Blob Data Reader"
-  principal_id         = var.fa_rumpole_gateway_identity_principal_id
-}
+# resource "azurerm_role_assignment" "ra_blob_data_reader" {
+#   scope                = azurerm_storage_container.container.resource_manager_id
+#   role_definition_name = "Storage Blob Data Reader"
+#   principal_id         = var.fa_rumpole_gateway_identity_principal_id
+# }
