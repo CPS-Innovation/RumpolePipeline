4c261ef3ba349082f7a7894e9f7b80a8914a5927
marty balmer <martybalmer@martys-air.home>
2022-05-18 13:04:11 +0100

    fixed config

--- terraform/functions-pdf-generator.tf
+++ terraform/functions-pdf-generator.tf
@@ -18,7 +18,7 @@ resource "azurerm_function_app" "fa_pdf_generator" {
     "BlobServiceUrl"                          = "https://sacps${var.env != "prod" ? var.env : ""}rumpolepipeline.blob.core.windows.net/"
     "BlobServiceContainerName"                = "documents"
     "StubBlobStorageConnectionString"         = var.stub_blob_storage_connection_string,
-    "AuthorizationClaim"                      = azuread_application_app_role.ar_pdf_generator_creator.value
+    "AuthorizationClaim"                      = "application.create" //TODO reference app role value
   }
   site_config {
     always_on      = true
