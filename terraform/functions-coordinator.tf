9549605fa42bc974152618dc2a49d430b88187fa
aceltuk <mark.jones3@cps.gov.uk>
2022-09-08 15:29:20 +0100

    Explicitly set client_secret_setting_name to empty value, now necessary when reverting from v2 to v1 for auth

--- terraform/functions-coordinator.tf
+++ terraform/functions-coordinator.tf
@@ -51,9 +51,10 @@ resource "azurerm_function_app" "fa_coordinator" {
     unauthenticated_client_action = "RedirectToLoginPage"
     default_provider              = "AzureActiveDirectory"
     active_directory {
-      client_id         = azuread_application.fa_coordinator.application_id
-      client_secret     = azuread_application_password.faap_fa_coordinator_app_service.value
-      allowed_audiences = ["api://fa-${local.resource_name}-coordinator"]
+      client_id                   = azuread_application.fa_coordinator.application_id
+      client_secret               = azuread_application_password.faap_fa_coordinator_app_service.value
+      client_secret_setting_name  = ""
+      allowed_audiences           = ["api://fa-${local.resource_name}-coordinator"]
     }
   }
   
