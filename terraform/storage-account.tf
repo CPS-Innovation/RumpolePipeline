92e23527c5a90c12dd57eed25cb6619c4601a405
Mark Jones <aceltuk@users.noreply.github.com>
2022-09-12 18:30:02 +0100

    Update storage-account.tf

--- terraform/storage-account.tf
+++ terraform/storage-account.tf
@@ -25,6 +25,11 @@ resource "azurerm_storage_account_customer_managed_key" "ok_cmk" {
   storage_account_id = azurerm_storage_account.sa.id
   key_vault_id       = azurerm_key_vault.kv.id
   key_name           = azurerm_key_vault_key.kvap_sa_customer_managed_key.name
+  
+  depends_on = [
+    azurerm_role_assignment.kv_role_client_kvc,
+    azurerm_role_assignment.kv_role_sa_kvcseu,
+  ]
 }
 
 resource "azurerm_role_assignment" "ra_blob_delegator_text_extractor" {
