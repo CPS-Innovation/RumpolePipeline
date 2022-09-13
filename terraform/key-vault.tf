5e96f1a569fa934bd8c10b3eb2cab7d54d37ef9f
aceltuk <mark.jones3@cps.gov.uk>
2022-09-13 15:53:08 +0100

    Revert keyvault changes

--- terraform/key-vault.tf
+++ terraform/key-vault.tf
@@ -5,27 +5,52 @@ resource "azurerm_key_vault" "kv" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   tenant_id           = data.azurerm_client_config.current.tenant_id
-  enabled_for_template_deployment = true
-  enabled_for_disk_encryption     = true
-  enable_rbac_authorization       = true
-  purge_protection_enabled        = true
-  soft_delete_retention_days      = 90
 
   sku_name = "standard"
 }
 
-resource "azurerm_key_vault_key" "kvap_sa_customer_managed_key" {
-  name         = "tfex-key"
+resource "azurerm_key_vault_access_policy" "kvap_fa_coordinator" {
   key_vault_id = azurerm_key_vault.kv.id
-  key_type     = "RSA"
-  key_size     = 2048
-  key_opts     = ["decrypt", "encrypt", "sign", "unwrapKey", "verify", "wrapKey"]
-  expiration_date = timeadd(timestamp(), "8760h")
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = azurerm_function_app.fa_coordinator.identity[0].principal_id
 
-  depends_on = [
-    azurerm_role_assignment.kv_role_terraform_sp,
-    azurerm_role_assignment.kv_role_client_kvc,
-    azurerm_role_assignment.kv_role_sa_kvcseu
+  secret_permissions = [
+    "Get",
+  ]
+
+}
+
+resource "azurerm_key_vault_access_policy" "kvap_fa_pdf_generator" {
+  key_vault_id = azurerm_key_vault.kv.id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = azurerm_function_app.fa_pdf_generator.identity[0].principal_id
+
+  secret_permissions = [
+    "Get",
+  ]
+}
+
+resource "azurerm_key_vault_access_policy" "kvap_fa_text_extractor" {
+  key_vault_id = azurerm_key_vault.kv.id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = azurerm_function_app.fa_text_extractor.identity[0].principal_id
+
+  secret_permissions = [
+    "Get",
+  ]
+}
+
+resource "azurerm_key_vault_access_policy" "kvap_terraform_sp" {
+  key_vault_id = azurerm_key_vault.kv.id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = data.azuread_service_principal.terraform_service_principal.object_id
+
+  secret_permissions = [
+    "Get",
+    "Set",
+    "Delete",
+    "Purge",
+    "Recover"
   ]
 }
 
@@ -34,62 +59,6 @@ resource "azurerm_key_vault_secret" "kvs_fa_coordinator_client_secret" {
   value        = azuread_application_password.faap_fa_coordinator_app_service.value
   key_vault_id = azurerm_key_vault.kv.id
   depends_on = [
-    azurerm_role_assignment.kv_role_terraform_sp,
-    azurerm_role_assignment.kv_role_client_kvc,
-    azurerm_role_assignment.kv_role_sa_kvcseu
+    azurerm_key_vault_access_policy.kvap_terraform_sp
   ]
-}
-
-resource "azurerm_role_assignment" "kv_role_terraform_sp" {
-  scope                = azurerm_key_vault.kv.id
-  role_definition_name = "Key Vault Administrator"
-  principal_id         = data.azuread_service_principal.terraform_service_principal.object_id
-}
-
-resource "azurerm_role_assignment" "kv_role_client_kvc" {
-  scope                = azurerm_key_vault.kv.id
-  role_definition_name = "Key Vault Administrator"
-  principal_id         = data.azurerm_client_config.current.object_id
-}
-
-resource "azurerm_role_assignment" "kv_role_sa_kvcseu" {
-  scope                = azurerm_key_vault.kv.id
-  role_definition_name = "Key Vault Crypto Service Encryption User"
-  principal_id         = azurerm_storage_account.sa.identity.0.principal_id
-}
-
-resource "azurerm_role_assignment" "kv_role_fa_coordinator_crypto_user" {
-  scope                = azurerm_key_vault.kv.id
-  role_definition_name = "Key Vault Crypto User"
-  principal_id         = azurerm_function_app.fa_coordinator.identity[0].principal_id
-}
-
-resource "azurerm_role_assignment" "kv_role_fa_coordinator_secrets_user" {
-  scope                = azurerm_key_vault.kv.id
-  role_definition_name = "Key Vault Secrets User"
-  principal_id         = azurerm_function_app.fa_coordinator.identity[0].principal_id
-}
-
-resource "azurerm_role_assignment" "kv_role_fa_pdf_generator_crypto_user" {
-  scope                = azurerm_key_vault.kv.id
-  role_definition_name = "Key Vault Crypto User"
-  principal_id         = azurerm_function_app.fa_pdf_generator.identity[0].principal_id
-}
-
-resource "azurerm_role_assignment" "kv_role_fa_pdf_generator_secrets_user" {
-  scope                = azurerm_key_vault.kv.id
-  role_definition_name = "Key Vault Secrets User"
-  principal_id         = azurerm_function_app.fa_pdf_generator.identity[0].principal_id
-}
-
-resource "azurerm_role_assignment" "kv_role_fa_text_extractor_crypto_user" {
-  scope                = azurerm_key_vault.kv.id
-  role_definition_name = "Key Vault Crypto User"
-  principal_id         = azurerm_function_app.fa_text_extractor.identity[0].principal_id
-}
-
-resource "azurerm_role_assignment" "kv_role_fa_text_extractor_secrets_user" {
-  scope                = azurerm_key_vault.kv.id
-  role_definition_name = "Key Vault Secrets User"
-  principal_id         = azurerm_function_app.fa_text_extractor.identity[0].principal_id
 }
--- terraform/storage-account.tf
+++ terraform/storage-account.tf
@@ -7,32 +7,12 @@ resource "azurerm_storage_account" "sa" {
   account_replication_type  = "LRS"
   account_tier              = "Standard"
   enable_https_traffic_only = true
-  shared_access_key_enabled = true
-  allow_blob_public_access  = false
 
   min_tls_version = "TLS1_2"
 
   network_rules {
-    default_action = "Deny"
+    default_action = "Allow"
   }
-
-  identity {
-    type = "SystemAssigned"
-  }
-}
-
-resource "azurerm_storage_account_customer_managed_key" "rumpole_storage_pipeline_cmk" {
-  storage_account_id = azurerm_storage_account.sa.id
-  key_vault_id       = azurerm_key_vault.kv.id
-  key_name           = azurerm_key_vault_key.kvap_sa_customer_managed_key.name
-  
-  depends_on = [
-    azurerm_role_assignment.kv_role_client_kvc,
-    azurerm_role_assignment.kv_role_sa_kvcseu,
-    azurerm_key_vault_key.kvap_sa_customer_managed_key,
-    azurerm_storage_account.sa,
-    azurerm_role_assignment.kv_role_terraform_sp
-  ]
 }
 
 resource "azurerm_role_assignment" "ra_blob_delegator_text_extractor" {
@@ -45,16 +25,6 @@ resource "azurerm_storage_container" "container" {
   name                  = "documents"
   storage_account_name  = azurerm_storage_account.sa.name
   container_access_type = "private"
-}
-
-resource "azurerm_log_analytics_storage_insights" "asi" {
-  name                = "sacps${var.env != "prod" ? var.env : ""}documentinsights"
-  resource_group_name = azurerm_resource_group.rg.name
-  workspace_id        = azurerm_log_analytics_workspace.law.id
-
-  storage_account_id  = azurerm_storage_account.sa.id
-  storage_account_key = azurerm_storage_account.sa.primary_access_key
-  blob_container_names= [azurerm_storage_container.container.name]
 }
 
 resource "azurerm_role_assignment" "ra_blob_data_contributor" {
