42d4844abffea51cd03f4e9eb221f44273d0d8bb
Mark Jones <aceltuk@users.noreply.github.com>
2022-09-13 11:56:32 +0100

    Ensure Azure storage account logging for blobs is enabled (#51)

--- terraform/application-insights.tf
+++ terraform/application-insights.tf
@@ -1,4 +1,4 @@
-#################### Application Insights ####################
+#################### Application Insights and Log Analytics Workspaces ####################
 
 resource "azurerm_application_insights" "ai" {
   name                = "ai-${local.resource_name}"
@@ -6,4 +6,12 @@ resource "azurerm_application_insights" "ai" {
   resource_group_name = azurerm_resource_group.rg.name
   application_type    = "web"
   retention_in_days   = 90
+}
+
+resource "azurerm_log_analytics_workspace" "analytics_workspace_ok" {
+  name                = "exampleworkspace"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
 }
