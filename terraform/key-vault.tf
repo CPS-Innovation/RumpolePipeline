189ac5a3f77f46228f5804a561cc505ed8202483
Mark Jones <aceltuk@users.noreply.github.com>
2022-09-13 22:58:30 +0100

    Bug/more terraform tweaks initiated by code scanner results (#55)
    * Ensure the storage container storing the activity logs is not publicly accessible
    Ensure Storage logging is enabled for Queue service for read, write and delete requests
    Ensure default network access rule for Storage Accounts is set to deny
    Ensure that Azure Cognitive Search disables public network access
    
    * Update storage-account.tf
    
    * Update key-vault.tf

--- terraform/key-vault.tf
+++ terraform/key-vault.tf
@@ -5,54 +5,27 @@ resource "azurerm_key_vault" "kv" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   tenant_id           = data.azurerm_client_config.current.tenant_id
-  soft_delete_retention_days  = 90
-  purge_protection_enabled    = true
+  enabled_for_template_deployment = true
+  enabled_for_disk_encryption     = true
+  enable_rbac_authorization       = true
+  purge_protection_enabled        = true
+  soft_delete_retention_days      = 90
 
   sku_name = "standard"
 }
 
-resource "azurerm_key_vault_access_policy" "kvap_fa_coordinator" {
+resource "azurerm_key_vault_key" "kvap_sa_customer_managed_key" {
+  name         = "tfex-key"
   key_vault_id = azurerm_key_vault.kv.id
-  tenant_id    = data.azurerm_client_config.current.tenant_id
-  object_id    = azurerm_function_app.fa_coordinator.identity[0].principal_id
+  key_type     = "RSA"
+  key_size     = 2048
+  key_opts     = ["decrypt", "encrypt", "sign", "unwrapKey", "verify", "wrapKey"]
+  expiration_date = timeadd(timestamp(), "8760h")
 
-  secret_permissions = [
-    "Get",
-  ]
-
-}
-
-resource "azurerm_key_vault_access_policy" "kvap_fa_pdf_generator" {
-  key_vault_id = azurerm_key_vault.kv.id
-  tenant_id    = data.azurerm_client_config.current.tenant_id
-  object_id    = azurerm_function_app.fa_pdf_generator.identity[0].principal_id
-
-  secret_permissions = [
-    "Get",
-  ]
-}
-
-resource "azurerm_key_vault_access_policy" "kvap_fa_text_extractor" {
-  key_vault_id = azurerm_key_vault.kv.id
-  tenant_id    = data.azurerm_client_config.current.tenant_id
-  object_id    = azurerm_function_app.fa_text_extractor.identity[0].principal_id
-
-  secret_permissions = [
-    "Get",
-  ]
-}
-
-resource "azurerm_key_vault_access_policy" "kvap_terraform_sp" {
-  key_vault_id = azurerm_key_vault.kv.id
-  tenant_id    = data.azurerm_client_config.current.tenant_id
-  object_id    = data.azuread_service_principal.terraform_service_principal.object_id
-
-  secret_permissions = [
-    "Get",
-    "Set",
-    "Delete",
-    "Purge",
-    "Recover"
+  depends_on = [
+    azurerm_role_assignment.kv_role_terraform_sp,
+    azurerm_role_assignment.kv_role_client_kvc,
+    azurerm_role_assignment.kv_role_sa_kvcseu
   ]
 }
 
@@ -61,6 +34,62 @@ resource "azurerm_key_vault_secret" "kvs_fa_coordinator_client_secret" {
   value        = azuread_application_password.faap_fa_coordinator_app_service.value
   key_vault_id = azurerm_key_vault.kv.id
   depends_on = [
-    azurerm_key_vault_access_policy.kvap_terraform_sp
+    azurerm_role_assignment.kv_role_terraform_sp,
+    azurerm_role_assignment.kv_role_client_kvc,
+    azurerm_role_assignment.kv_role_sa_kvcseu
   ]
+}
+
+resource "azurerm_role_assignment" "kv_role_terraform_sp" {
+  scope                = azurerm_key_vault.kv.id
+  role_definition_name = "Key Vault Administrator"
+  principal_id         = data.azuread_service_principal.terraform_service_principal.object_id
+}
+
+resource "azurerm_role_assignment" "kv_role_client_kvc" {
+  scope                = azurerm_key_vault.kv.id
+  role_definition_name = "Key Vault Administrator"
+  principal_id         = data.azurerm_client_config.current.object_id
+}
+
+resource "azurerm_role_assignment" "kv_role_sa_kvcseu" {
+  scope                = azurerm_key_vault.kv.id
+  role_definition_name = "Key Vault Crypto Service Encryption User"
+  principal_id         = azurerm_storage_account.sa.identity.0.principal_id
+}
+
+resource "azurerm_role_assignment" "kv_role_fa_coordinator_crypto_user" {
+  scope                = azurerm_key_vault.kv.id
+  role_definition_name = "Key Vault Crypto User"
+  principal_id         = azurerm_function_app.fa_coordinator.identity[0].principal_id
+}
+
+resource "azurerm_role_assignment" "kv_role_fa_coordinator_secrets_user" {
+  scope                = azurerm_key_vault.kv.id
+  role_definition_name = "Key Vault Secrets User"
+  principal_id         = azurerm_function_app.fa_coordinator.identity[0].principal_id
+}
+
+resource "azurerm_role_assignment" "kv_role_fa_pdf_generator_crypto_user" {
+  scope                = azurerm_key_vault.kv.id
+  role_definition_name = "Key Vault Crypto User"
+  principal_id         = azurerm_function_app.fa_pdf_generator.identity[0].principal_id
+}
+
+resource "azurerm_role_assignment" "kv_role_fa_pdf_generator_secrets_user" {
+  scope                = azurerm_key_vault.kv.id
+  role_definition_name = "Key Vault Secrets User"
+  principal_id         = azurerm_function_app.fa_pdf_generator.identity[0].principal_id
+}
+
+resource "azurerm_role_assignment" "kv_role_fa_text_extractor_crypto_user" {
+  scope                = azurerm_key_vault.kv.id
+  role_definition_name = "Key Vault Crypto User"
+  principal_id         = azurerm_function_app.fa_text_extractor.identity[0].principal_id
+}
+
+resource "azurerm_role_assignment" "kv_role_fa_text_extractor_secrets_user" {
+  scope                = azurerm_key_vault.kv.id
+  role_definition_name = "Key Vault Secrets User"
+  principal_id         = azurerm_function_app.fa_text_extractor.identity[0].principal_id
 }
