f5794fe98e245ac0b8bf02af44bb76862fc98074
aceltuk <mark.jones3@cps.gov.uk>
2022-09-13 23:49:22 +0100

    missing change

--- terraform/storage-account.tf
+++ terraform/storage-account.tf
@@ -26,7 +26,7 @@ resource "azurerm_storage_account_customer_managed_key" "rumpole_storage_pipelin
 
   depends_on = [
     azurerm_role_assignment.kv_role_client_kvc,
-    azurerm_role_assignment.kv_role_sa_kvcseu,
+    #azurerm_role_assignment.kv_role_sa_kvcseu,
     azurerm_key_vault_key.kvap_sa_customer_managed_key,
     azurerm_storage_account.sa,
     azurerm_role_assignment.kv_role_terraform_sp
