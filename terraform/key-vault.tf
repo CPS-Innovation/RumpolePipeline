26b5136bea113356d90473abf88c3bc640ce9e6b
aceltuk <mark.jones3@cps.gov.uk>
2022-09-13 16:51:01 +0100

    Corrected keyvault config

--- terraform/key-vault.tf
+++ terraform/key-vault.tf
@@ -5,6 +5,8 @@ resource "azurerm_key_vault" "kv" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   tenant_id           = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days  = 90
+  purge_protection_enabled    = true
 
   sku_name = "standard"
 }
