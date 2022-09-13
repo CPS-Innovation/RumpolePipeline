b0c8212c2d8e54c88cd094311ce420c1d464d5b1
Mark Jones <aceltuk@users.noreply.github.com>
2022-09-13 12:22:09 +0100

    Update resource-group.tf

--- terraform/resource-group.tf
+++ terraform/resource-group.tf
@@ -4,3 +4,11 @@ resource "azurerm_resource_group" "rg" {
   name     = "rg-${local.resource_name}"
   location = "UK South"
 }
+
+resource "azurerm_log_analytics_workspace" "law" {
+  name                = var.default_workspace
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+}
