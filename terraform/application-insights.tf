795c5c26899e6c94f8fd52174127db53db09e842
aceltuk <mark.jones3@cps.gov.uk>
2022-09-14 09:51:52 +0100

    Blob storage logging has to be set manually at the moment

--- terraform/application-insights.tf
+++ terraform/application-insights.tf
@@ -7,11 +7,3 @@ resource "azurerm_application_insights" "ai" {
   application_type    = "web"
   retention_in_days   = 90
 }
-
-resource "azurerm_log_analytics_workspace" "law" {
-  name                = var.default_workspace_name
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  sku                 = "PerGB2018"
-  retention_in_days   = 30
-}
