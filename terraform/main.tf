186339113a22e228c881fca42f6a8f8f7078efa9
aceltuk <mark.jones3@cps.gov.uk>
2022-11-09 23:21:54 +0000

    Reverted flags

--- terraform/main.tf
+++ terraform/main.tf
@@ -44,8 +44,8 @@ terraform {
 provider "azurerm" {
   features {
     key_vault {
-      recover_soft_deleted_key_vaults = false
-      purge_soft_delete_on_destroy    = false
+      recover_soft_deleted_key_vaults = true
+      purge_soft_delete_on_destroy    = true
     }    
   }
 }
