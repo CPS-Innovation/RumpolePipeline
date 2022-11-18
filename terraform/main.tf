a5e7b0d656eb9df38adad12259cc63f49bebae6d
aceltuk <mark.jones3@cps.gov.uk>
2022-11-18 10:18:21 +0000

    Quick correction

--- terraform/main.tf
+++ terraform/main.tf
@@ -24,12 +24,12 @@ terraform {
     }
   }
 
-  /*backend "azurerm" {
+  backend "azurerm" {
     storage_account_name = "__terraform_storage_account__"
     container_name       = "__terraform_container_name__"
     key                  = "__terraform_key__"
     access_key           = "__storage_key__"
-  }*/
+  }
   
   /*backend "azurerm" {
     resource_group_name  = "rg-terraform"
