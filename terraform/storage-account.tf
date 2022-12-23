35ca0af436fc321ea09d3981f2014fb43568b51d
aceltuk <mark.jones3@cps.gov.uk>
2022-12-23 11:41:10 +0000

    Removed unsupported tags definition from "azurerm_eventgrid_system_topic_event_subscription"

--- terraform/storage-account.tf
+++ terraform/storage-account.tf
@@ -138,8 +138,5 @@ resource "azurerm_eventgrid_system_topic_event_subscription" "pipeline_document_
   }
 
   included_event_types = ["Blob Deleted"]
-  tags = {
-    environment = "${var.env}"
-  }
   depends_on = [azurerm_storage_account.sa,azurerm_storage_management_policy.pipeline-documents-lifecycle,azurerm_eventgrid_system_topic.pipeline_document_deleted_topic]
 }
